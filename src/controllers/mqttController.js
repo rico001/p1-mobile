31fe4d42af89813c2e64f3dea00505fadc49267e
Rico Eisenberg <44041623+rico001@users.noreply.github.com>
2025-04-28 22:27:22 +0200

    * improve mqtt proxy

--- src/controllers/mqttController.js
+++ src/controllers/mqttController.js
@@ -13,11 +13,23 @@ export async function printFile3mf(req, res, next) {
 
 export async function getAccessCode(req, res, next) {
   try {
-    console.log('→ getAccessCode gestartet');
     const sequence_id = `access-code__${Date.now()}`;
-    const payload = { system: { sequence_id, command: 'get_access_code' } };
-    await mqttService.publish(mqttService.topics.request, payload);
-    res.json({ message: 'Access Code Request gesendet', sequence_id });
+    const payload = {
+      system: { sequence_id, command: 'get_access_code' }
+    };
+
+    // send & wait for report
+    const report = await mqttService.request(
+      mqttService.topics.request,
+      payload,
+      /* optional: Timeout in ms */ 10000
+    );
+
+    // Jetzt hast du die komplette Report-Nachricht
+    res.json({
+      report
+    });
+
   } catch (err) {
     next(err);
   }
