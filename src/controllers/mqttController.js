6fb49f4fee0bc1846301939ff7c7b32d1e2cf50b
Rico Eisenberg <44041623+rico001@users.noreply.github.com>
2025-05-02 16:16:25 +0200

    * update version

--- src/controllers/mqttController.js
+++ src/controllers/mqttController.js
@@ -229,7 +229,11 @@ export async function stopPrint(req, res, next) {
   try {
     const sequence_id = `print-stop__${Date.now()}`;
     const payload = {
-      print: { sequence_id, command: 'stop', param: '' }
+      print: { 
+        sequence_id, 
+        command: 'stop', 
+        param: '' 
+      }
     };
     const report = await mqttService.request(payload, sequence_id);
     res.json({ report });
@@ -242,7 +246,11 @@ export async function pausePrint(req, res, next) {
   try {
     const sequence_id = `print-pause__${Date.now()}`;
     const payload = {
-      print: { sequence_id, command: 'pause', param: '' }
+      print: { 
+        sequence_id, 
+        command: 'pause', 
+        param: '' 
+      }
     };
     const report = await mqttService.request(payload, sequence_id);
     res.json({ report });
@@ -255,7 +263,11 @@ export async function resumePrint(req, res, next) {
   try {
     const sequence_id = `print-resume__${Date.now()}`;
     const payload = {
-      print: { sequence_id, command: 'resume', param: '' }
+      print: { 
+        sequence_id, 
+        command: 'resume', 
+        param: '' 
+      }
     };
     const report = await mqttService.request(payload, sequence_id);
     res.json({ report });
