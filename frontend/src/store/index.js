6160e219ce7fed660f252d142d2f601ae9228b3f
Rico Eisenberg <44041623+rico001@users.noreply.github.com>
2025-05-04 09:16:41 +0200

    * impl on off switch for tasmota switch 0.0.10

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import printerReducer from './printerSlice';
+import tasmotaReducer from './tasmotaSlice';
 
 const store = configureStore({
   reducer: {
     printer: printerReducer,
+    tasmota: tasmotaReducer,
   },
 });
 
