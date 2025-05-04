a171cf486df83c16f219aae93a436262c23c5924
Rico Eisenberg <44041623+rico001@users.noreply.github.com>
2025-05-04 21:07:14 +0200

    * add cam switch

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import printerReducer from './printerSlice';
 import tasmotaReducer from './tasmotaSlice';
+import uiReducesr from './uiSlice';
 
 const store = configureStore({
   reducer: {
     printer: printerReducer,
     tasmota: tasmotaReducer,
+    ui: uiReducesr,
   },
 });
 
