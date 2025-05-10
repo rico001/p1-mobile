7b312a1ff479d875c1586e2eb3b37d8af9fd9ef6
Rico Eisenberg <44041623+rico001@users.noreply.github.com>
2025-05-10 03:57:00 +0200

    * fix reloading bug

--- frontend/src/components/PrinterStream.jsx
+++ frontend/src/components/PrinterStream.jsx
@@ -2,31 +2,35 @@ import React, { useState, useRef } from 'react';
 import { Box, CircularProgress, Dialog, DialogContent, IconButton } from '@mui/material';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import LightToggle from './LightToggle';
+const transparentImg = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAgKAAAAACH5BAUAAAAALAAAAAABAAEAAAICRAEAOw==';
 
 export default function PrinterStream(props) {
   const baseSrc = "/api/video/video-stream";
   const [reloadKey, setReloadKey] = useState(Date.now());
   const [previewOpen, setPreviewOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
   const fullscreenRef = useRef(null);
-  const [reloading, setReloading] = useState(true);
 
   const handleOpen = () => setPreviewOpen(true);
   const handleClose = () => setPreviewOpen(false);
 
   const handleError = (e) => {
     e.currentTarget.onerror = null;
+    e.currentTarget.src = transparentImg;
   };
 
-  const src = `${baseSrc}?reload=${reloadKey}`;
+  const src = loading
+  ? transparentImg
+  : `${baseSrc}?reload=${reloadKey}`;
 
   const reloadStream = () => {
-    if (reloading) return;
-    setReloading(true);
+    if (loading) return;
+    setLoading(true);
     setTimeout(() => {
       setReloadKey(Date.now());
     }, 1500);
     setTimeout(() => {
-      setReloading(false);
+      setLoading(false);
     }, 6000);
   };
 
@@ -50,13 +54,6 @@ export default function PrinterStream(props) {
           alt="Printer Stream Preview"
           onError={handleError}
           onClick={handleOpen}
-          onLoad={() => {
-            setReloading(false);
-          }}
-          onLoadStart={() => {
-            setReloading(true);
-          }
-          }
           sx={{
             width: '100%',
             height: '100%',
@@ -64,7 +61,7 @@ export default function PrinterStream(props) {
             background: '#4040404a',
           }}
         />
-        {reloading && (
+        {loading && (
           <Box
             sx={{
               position: 'absolute',
