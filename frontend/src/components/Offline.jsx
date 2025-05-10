137416701158c7dd3c680878fd1e65e3dd6c8b27
Rico Eisenberg <44041623+rico001@users.noreply.github.com>
2025-05-11 00:01:06 +0200

    * wifi_signal_update to online on connect to mqtt server

--- frontend/src/components/Offline.jsx
+++ frontend/src/components/Offline.jsx
@@ -1,20 +1,31 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import TasmotaSwitch from "./TasmotaSwitch";
 
 export const OfflineState = () => {
     return (
         <div
             style={{
-                margin: 'auto',
+                margin: '30px',
                 textAlign: 'center',
                 height: '80%',
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
+                flexDirection: 'column'
             }}
         >
-            <Typography variant="h6" color="error">
-                Schalten Sie den Drucker ein oder überprüfen Sie die WLAN-Verbindung.
+            <Typography variant="h6" color="error" maxWidth={400} mb={2}>
+                Drucker ist offline
             </Typography>
+            <Typography variant="h6" color="error" maxWidth={400}>
+                Überprüfen Sie die Verbindung oder schalten Sie den Drucker ein.
+            </Typography>
+            <Typography variant="body2" color="white" mt={2} maxWidth={400}>
+                Es kann nach dem Einschalten des Druckers einige Sekunden dauern, bis die Verbindung wiederhergestellt ist.
+            </Typography>
+            <Box mt={2} display="flex" justifyContent="center" alignItems="center">
+                <TasmotaSwitch />
+            </Box>
         </div>
     );
 }
