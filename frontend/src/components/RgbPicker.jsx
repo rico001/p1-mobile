47d810b6b5f277159ddfdd1e0e6e775acb78c54e
Rico Eisenberg <44041623+rico001@users.noreply.github.com>
2025-05-18 13:47:44 +0200

    * add rgb picker for tray color

---
+++ frontend/src/components/RgbPicker.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { RgbColorPicker } from 'react-colorful';
+//import 'react-colorful/dist/index.css';
+import { Box, TextField, Grid } from '@mui/material';
+
+const RgbPicker = ({ color, onChange }) => {
+  return (
+    <Box>
+      <RgbColorPicker color={color} onChange={onChange} />
+      <Grid container spacing={1} sx={{ mt: 1 }}>
+        {['r','g','b'].map((ch) => (
+          <Grid item xs={4} key={ch}>
+            <TextField
+              label={ch.toUpperCase()}
+              type="number"
+              inputProps={{ min: 0, max: 255 }}
+              fullWidth
+              value={color[ch]}
+              onChange={(e) => {
+                const v = Math.min(255, Math.max(0, Number(e.target.value) || 0));
+                onChange({ ...color, [ch]: v });
+              }}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+RgbPicker.propTypes = {
+  color: PropTypes.shape({
+    r: PropTypes.number.isRequired,
+    g: PropTypes.number.isRequired,
+    b: PropTypes.number.isRequired,
+  }).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default RgbPicker;
