0968ba98c37a908418d98088f5a3df03d6d02a53
Rico Eisenberg <44041623+rico001@users.noreply.github.com>
2025-05-20 23:13:17 +0200

    * update version

--- frontend/src/pages/AMS.jsx
+++ frontend/src/pages/AMS.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import AMSState from '../components/AMSState';
+import { Box } from '@mui/material';
 
 function AMS() {
     return (
-        <>
+        <Box p={2} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <AMSState />
-        </>
+        </Box>
     );
 }
 
