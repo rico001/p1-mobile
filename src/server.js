9a5e048b803dcd98fff6e9435aa4bad4b21ebfb8
Rico Eisenberg <44041623+rico001@users.noreply.github.com>
2025-06-06 20:12:37 +0200

    * add MQTT proxy service and proxy broker logic * update version

--- src/server.js
+++ src/server.js
@@ -3,11 +3,14 @@ import app from './app.js';
 import { config } from './config/index.js';
 import mqttService from './services/mqttService.js';
 import websocketService from './services/websocketService.js';
+import mqttProxyService from './services/mqttProxyService.js';
 
 const server = http.createServer(app);
 
 // Services initialisieren
-mqttService.init();
+mqttProxyService.init();
+mqttService.init(mqttProxyService);
+
 websocketService.init(server);
 
 // Beispiel: zyklisches Broadcasting
